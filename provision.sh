d7c9408c59b75c53010560da5ad0b45568ac99a0
Christian Nuss <cnuss@users.noreply.github.com>
2019-02-25 14:55:20 -0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -7,3 +7,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get -q -y install puppet git
+
+git clone https://github.com/cnuss/chromebook-provision.git /opt/chromebook-provision || true
+rm -fr /etc/puppet
+ln -s /opt/chromebook-provision/etc/puppet /etc/puppet
+
+pushd /etc/puppet
+git pull
+popd
+
+#puppet apply /etc/puppet/manifests/site.pp
