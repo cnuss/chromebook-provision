34b469a34f452b29a10a54f7cb0b583427906e44
Christian Nuss <cnuss@users.noreply.github.com>
2019-02-26 23:30:36 -0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -5,10 +5,16 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+# Add Ansible repo
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+
+# Install prereqs
 apt-get update
 apt-get -q -y install ansible git
 
+# Get this repo
 git clone https://github.com/cnuss/chromebook-provision.git /opt/chromebook-provision 2> /dev/null || (cd /opt/chromebook-provision; git pull)
 
+# Begin provisioning
 ansible-playbook -K /opt/chromebook-provision/ansible/provision.yml
