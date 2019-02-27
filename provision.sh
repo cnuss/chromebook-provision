eb4ae501351385e443a694a98832f67a9b9f6c17
Christian Nuss <cnuss@users.noreply.github.com>
2019-02-26 23:27:32 -0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,16 +4,11 @@ set -e
 set -x
 
 export DEBIAN_FRONTEND=noninteractive
-export PWD=$(pwd)
 
-sudo apt-get update
-sudo apt-get -q -y install puppet git
+deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+apt-get update
+apt-get -q -y install ansible git
 
 git clone https://github.com/cnuss/chromebook-provision.git /opt/chromebook-provision 2> /dev/null || (cd /opt/chromebook-provision; git pull)
-rm -fr /etc/puppet
-ln -s /opt/chromebook-provision/etc/puppet /etc/puppet
 
-cd /etc/puppet
-puppet apply /etc/puppet/manifests/site.pp
-
-cd "$PWD"
+ansible-playbook -K /opt/chromebook-provision/ansible/provision.yml
