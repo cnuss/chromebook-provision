2360b8bd802c765c55919b15b1ba0d03074eba65
Christian Nuss <cnuss@users.noreply.github.com>
2019-02-27 22:28:44 -0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -8,21 +8,23 @@ BASE_REQS=/opt/base-reqs
 
 if [ -z "$BASE_REQS" ]; then
   # Add Ansible repo
-  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
   # Install prereqs
-  apt-get update
-  apt-get -q -y install ansible git
+  sudo apt-get update
+  sudo apt-get -q -y install ansible git
   
-  git clone https://github.com/cnuss/chromebook-provision.git /opt/chromebook-provision
+  sudo git clone https://github.com/cnuss/chromebook-provision.git /opt/chromebook-provision
 fi
 
 touch "$BASE_REQS"
 
 # Get this repo
 cd /opt/chromebook-provision
-git pull
+sudo git pull
 
 # Begin provisioning
-ansible-playbook /opt/chromebook-provision/ansible/provision.yml
+sudo ansible-playbook /opt/chromebook-provision/ansible/provision.yml
+
+exec $SHELL
